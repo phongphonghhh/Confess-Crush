336715b77a63a367b1b0fa6e9e078ab08d22c7e4
phongphonghhh <98164720+phongphonghhh@users.noreply.github.com>
2022-01-21 21:35:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,6 @@ Vì chẳng có đôi nào đẹp bằng đôi ta`,
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì cậu đặc biệt',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: https://www.facebook.com/messages/t/100027850055187
+   
