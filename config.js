f841261a1faab2255bb17108027f7bcac274d26d
phongphonghhh <98164720+phongphonghhh@users.noreply.github.com>
2022-01-21 20:10:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
+    titleWeb: "Yêu Minh Sao",
     introTitle: 'Tên người yêu bạn',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
